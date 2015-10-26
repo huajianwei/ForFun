c3715da09dcfca35efb97d25bc82268673c182de
panyantao <hwamei7@163.com>
2015-10-26 12:11:54 +0800

    new file:   forFun/forFun.xcodeproj/project.pbxproj 	new file:   forFun/forFun.xcodeproj/project.xcworkspace/contents.xcworkspacedata 	new file:   forFun/forFun.xcodeproj/project.xcworkspace/xcuserdata/panyantao.xcuserdatad/UserInterfaceState.xcuserstate 	new file:   forFun/forFun.xcodeproj/xcuserdata/panyantao.xcuserdatad/xcschemes/forFun.xcscheme 	new file:   forFun/forFun.xcodeproj/xcuserdata/panyantao.xcuserdatad/xcschemes/xcschememanagement.plist 	new file:   forFun/forFun/AppDelegate.h 	new file:   forFun/forFun/AppDelegate.m 	new file:   forFun/forFun/Assets.xcassets/AppIcon.appiconset/Contents.json 	new file:   forFun/forFun/Base.lproj/LaunchScreen.storyboard 	new file:   forFun/forFun/Base.lproj/Main.storyboard 	new file:   forFun/forFun/Info.plist 	new file:   forFun/forFun/ViewController.h 	new file:   forFun/forFun/ViewController.m 	new file:   forFun/forFun/main.m 	new file:   forFun/forFunTests/Info.plist 	new file:   forFun/forFunTests/forFunTests.m 	new file:   forFun/forFunUITests/Info.plist 	new file:   forFun/forFunUITests/forFunUITests.m

---
+++ forFun/forFun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  forFun
+//
+//  Created by panyantao on 15/10/26.
+//  Copyright © 2015年 hwamei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ forFun/forFun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  forFun
+//
+//  Created by panyantao on 15/10/26.
+//  Copyright © 2015年 hwamei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
