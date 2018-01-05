dec5da1d8129265e42b8349278af2d2181386e2d
huajianwei <adam@adamdeMacBook-Pro.local>
2018-01-05 17:14:04 +0800

    枚举类型和组合枚举类型代码测试

--- forFun/forFun/ViewController.h
+++ forFun/forFun/ViewController.h
@@ -10,6 +10,29 @@
 
 extern NSString *const HJWNoticeNSString;//前面声明的时候要显示一个extern的关键字，这是是在告诉编译器，在全局变量表中有一个HJWNoticeNSString的符号，编译器无需查看定义，即允许代码使用此常量，当链接成二进制之后，肯定能找到该常量。在该声明文件中声明后，需要在实现文件中定义 NSString *const HJWNoticeNSString = @"HJWNoticeNSString";这样来定义。只能定义一次。
 
+enum animationState:NSUInteger{
+    animationEnd,
+    animationStarting,
+    animationRunning,
+};
+typedef enum animationState animationState;
+
+typedef enum : NSUInteger {
+    HanimationEnd,
+    HanimationStarting,
+    HanimationRunning,
+} HanimationState;
+//这两种方式定义枚举类型是等价的。如何处理枚举类型是编译器进行处理的。不过第一种的枚举类型是编译器自己决定的，第二种是确定使用的NSUInteger类型。
+
+typedef enum : NSUInteger {
+    HjwViewAutoNone = 0,
+    HjwViewAutoFlexibleleft = 1<<0,
+    HjwViewAutoWidth = 1<<1,
+    HjwViewAutoFlexibleRight = 1<<2,
+    HjwViewAutoFlexibleTop = 1<<3,
+    HjwViewAutoHeight = 1<<4,
+    HjwViewAutoFlexibleBottom = 1<<5,
+} HjwViewAuto;//在使用的时候可以用按位或操作符来组合多个选项
 
 @interface ViewController : UIViewController
 
--- forFun/forFun/ViewController.m
+++ forFun/forFun/ViewController.m
@@ -11,6 +11,8 @@
 
 NSString *const HJWNoticeNSString = @"value";
 
+static const NSTimeInterval KTimeDuring = 2;//这样声明要比#define好，因为编译器会确保常量值不变。
+
 @interface ViewController ()
 
 @end
@@ -27,11 +29,28 @@ NSString *const HJWNoticeNSString = @"value";
     
     NSLog(@"%@",layer);
 
+
     FirstView *myview = [[FirstView alloc]initWithFrame:CGRectMake(0, 0, 320, 480)];
 
     myview.backgroundColor = [UIColor cyanColor];
 
     [self.view addSubview:myview];
+
+
+    //枚举值的使用
+    NSLog(@"%.f",KTimeDuring);
+
+    //使用|操作来组合使用枚举值
+
+    HjwViewAuto anto = HjwViewAutoWidth|HjwViewAutoHeight;
+    if (anto & HjwViewAutoHeight) {//使用枚举类型可以设置组合的类型，这里可以使用|&进行判断。简直好用到飞起。。。
+        NSLog(@"%ld",anto);
+    }
+    if (anto & HjwViewAutoNone) { //在这里编译器会提醒不会执行到这里
+        NSLog(@"%ld",anto);
+    }
+
+
 }
 
 - (void)didReceiveMemoryWarning {
